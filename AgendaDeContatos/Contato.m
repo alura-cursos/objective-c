9f1f820da7ba55306a680e0df10044a07366e865
Joviane <joviane@gmail.com>
2015-12-17 15:25:00 -0200

    [Aula 1] Instanciando um contato e guardando os valores

---
+++ AgendaDeContatos/Contato.m
@@ -0,0 +1,13 @@
+//
+//  Contato.m
+//  AgendaDeContatos
+//
+//  Created by Joviane Jardim on 12/17/15.
+//  Copyright © 2015 Alura. All rights reserved.
+//
+
+#import "Contato.h"
+
+@implementation Contato
+
+@end
--- AgendaDeContatos/ViewController.m
+++ AgendaDeContatos/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Contato.h"
 
 @interface ViewController ()
 
@@ -15,12 +16,13 @@
 @implementation ViewController
 
 - (IBAction) adiciona {
-    NSString *nome = self.nome.text;
-    NSString *endereco = self.endereco.text;
-    NSString *site = self.site.text;
-    NSString *telefone = self.telefone.text;
-    NSString *email = self.email.text;
-    NSLog(@"Dados do Contato: %@ %@ %@ %@ %@", nome, endereco, site, telefone, email);
+    Contato *contato = [Contato new];
+    contato.nome = self.nome.text;
+    contato.endereco = self.endereco.text;
+    contato.site = self.site.text;
+    contato.telefone = self.telefone.text;
+    contato.email = self.email.text;
+    NSLog(@"Dados do Contato: %@ %@ %@ %@ %@", contato.nome, contato.endereco, contato.site, contato.telefone, contato.email);
 }
 
 @end
