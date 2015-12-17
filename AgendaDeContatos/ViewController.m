ff35abfa0acd164c1d29aaac9ea3d7e42705fc26
Joviane <joviane@gmail.com>
2015-12-17 14:56:53 -0200

    [Aula 1] Criando método para adicionar com NSLog

--- AgendaDeContatos/ViewController.m
+++ AgendaDeContatos/ViewController.m
@@ -14,14 +14,8 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction) adiciona {
+    NSLog(@"Clicou no botão");
 }
 
 @end
