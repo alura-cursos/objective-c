d76c35238b9185502a58c90cb2c1685edf39290d
Joviane <joviane@gmail.com>
2015-12-17 16:31:49 -0200

    [Aula 2] Criando view controller para a lista de contatos

---
+++ AgendaDeContatos/ListaContatosViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListaContatosViewController.h
+//  AgendaDeContatos
+//
+//  Created by Joviane Jardim on 12/17/15.
+//  Copyright © 2015 Alura. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ListaContatosViewController : UITableViewController
+
+@end
---
+++ AgendaDeContatos/ListaContatosViewController.m
@@ -0,0 +1,19 @@
+//
+//  ListaContatosViewController.m
+//  AgendaDeContatos
+//
+//  Created by Joviane Jardim on 12/17/15.
+//  Copyright © 2015 Alura. All rights reserved.
+//
+
+#import "ListaContatosViewController.h"
+
+@interface ListaContatosViewController ()
+
+@end
+
+@implementation ListaContatosViewController
+
+
+
+@end
