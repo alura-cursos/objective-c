9c5409f4f35cce548e97a917e0db09e6a31387eb
Joviane <joviane@gmail.com>
2015-12-17 19:52:24 -0200

    [Aula 2] Colocando o botão do form no navigation controller dentro do initWithCoder e voltando pra tela da lista

--- AgendaDeContatos/ViewController.m
+++ AgendaDeContatos/ViewController.m
@@ -15,14 +15,26 @@
 
 @implementation ViewController
 
-- (IBAction) adiciona {
+-(id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        UIBarButtonItem *botao = [[UIBarButtonItem alloc] initWithTitle:@"Adicionar" style:UIBarButtonItemStylePlain target:self action:@selector(adiciona)];
+        
+        self.navigationItem.rightBarButtonItem = botao;
+        self.navigationItem.title = @"Novo Contato";
+    }
+    return self;
+}
+
+-(void) adiciona {
     Contato *contato = [Contato new];
     contato.nome = self.nome.text;
     contato.endereco = self.endereco.text;
     contato.site = self.site.text;
     contato.telefone = self.telefone.text;
     contato.email = self.email.text;
-    NSLog(@"Dados do Contato: %@ %@ %@ %@ %@", contato.nome, contato.endereco, contato.site, contato.telefone, contato.email);
+    
+    [self.navigationController popViewControllerAnimated:YES];
 }
 
 @end
